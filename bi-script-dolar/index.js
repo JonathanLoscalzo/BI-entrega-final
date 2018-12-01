620e90e7d7c8fb80a65e627a73ec35714c4c5d6b
agudeluca <agustin_deluca@hotmail.com>
2018-11-30 22:58:31 -0300

    ready

--- bi-script-dolar/index.js
+++ bi-script-dolar/index.js
@@ -1,25 +1,20 @@
 import axios from 'axios'
 import moment from 'moment'
 import fs from 'fs';
-const request = () => (
-    axios.get('https://bankersalgo.com/apihistoric2/5bfdb399edcc0/d6f8d1769410fc1fc1bd9fe65e36f99f/USD/2017-12-16').then(
-        data => ({ "date": data.data.date, "ARS": data.rates.ARS })
+const request = (date) => (
+    axios.get(`https://bankersalgo.com/apihistoric2/5bfdb399edcc0/d6f8d1769410fc1fc1bd9fe65e36f99f/USD/${date}`).then(
+        data => data.data.rates.ARS
     ).catch(err => null)
 )
 const init = async () => {
-    fs.writeFile('./data/temp.txt', '');
-    let startDate = moment(new Date(2018, 1, 1));
+    let startDate = moment(new Date(2017, 1, 1));
     let aux = 1
     const currentDay = moment(new Date());
     while (startDate < currentDay) {
-        // console.log(aux);
-        // console.log(startDate);
-        // console.log(moment(startDate).format("YYYY-MM-DD"));
-        fs.appendFile('./data/temp.txt', `${moment(startDate).format("YYYY-MM-DD")},${'dolarr'}\n`);
+        fs.appendFile('./data/temp.txt', `${moment(startDate).format("YYYY-MM-DD")},${await request(moment(startDate).format("YYYY-MM-DD"))}\n`);
         startDate = startDate.add(1, 'days')
         aux++
-
     }
-    console.log(await request())
+    // console.log(await request())
 }
 init()
